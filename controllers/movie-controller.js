76665a8286aed022abad24f5ddb238e063e2e647
alexlitinsky <litinsky.a@northeastern.edu>
2023-06-21 22:22:49 -0700

    create and find comments works, still need to fix update issue

--- controllers/movie-controller.js
+++ controllers/movie-controller.js
@@ -5,48 +5,26 @@ const MovieController = (app) => {
   const updateComment = async (req, res) => {
     const { movieId } = req.params;
     const { comment } = req.body;
-
-    try {
-      console.log("in movieController updateComment method");
-      const movie = await movieDao.updateComment(movieId, comment);
-      res.json(movie);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
-
+    console.log("in movieController updateComment method");
+    const movie = await movieDao.updateComment(movieId, comment);
+    res.json(movie);
   }
   const createComment = async (req, res) => {
-    const { movieId } = req.params;
-    const { comment } = req.body;
-
-    try {
-      console.log("in movieController createComment method");
-      const movie = await movieDao.createComment(movieId, comment);
-      res.json(movie);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
+    console.log("in movieController createComment method");
+    const movie = await movieDao.createComment(req.body);
+    res.json(movie);
   };
 
   const findComments = async (req, res) => {
     const { movieId } = req.params;
-
-    try {
-      console.log("in movieController findComments method");
-      const comments = await movieDao.findComments(movieId);
-      res.json(comments);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(500);
-    }
+    console.log("in movieController findComments method");
+    const comments = await movieDao.findComments(movieId);
+    res.json(comments);
   };
 
-  // Other controller methods...
 
   // another consideration, how to create and update comments in a single function?
-  app.post("/api/movies/comments/:movieId", createComment);
+  app.post("/api/movies/comments/", createComment);
   app.get("/api/movies/comments/:movieId", findComments);
   app.put("/api/movies/comments/:movieId", updateComment);
 
--- controllers/movie-schema.js
+++ controllers/movie-schema.js
@@ -4,7 +4,7 @@ const commentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    // required: true
   },
   rating: {
     type: Number,
