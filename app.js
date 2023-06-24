7b6570b845ce918cddc0b31c5de5b10978607dea
Sayali Oak <sayalioak@Sayalis-MacBook-Pro.local>
2023-06-24 15:12:11 -0700

    profile page acting weird

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import AuthController from './controllers/auth-controller.js';
 import MovieController from './controllers/movie-controller.js';
 
 
-// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/zinema' //'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/zinema'
-const CONNECTION_STRING = 'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/zinema'
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/zinema' //'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/zinema'
+// const CONNECTION_STRING = 'mongodb+srv://Cluster57626:anjali@cluster0.jyy3myf.mongodb.net/zinema'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
